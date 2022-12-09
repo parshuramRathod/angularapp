431fc6167511625743aa67e7f110372b8c1feefa
Parshuram <parshuramrathod9540@gmail.com>
2022-12-09 09:09:26 +0530

    learned formGroup and controls
    learned formGroup and controls

---
+++ src/app/models/employee.ts
@@ -0,0 +1,5 @@
+export class Employee{
+    course: string;
+    userName:string;
+    email:string;
+}
--- src/app/simpleform/simpleform.component.ts
+++ src/app/simpleform/simpleform.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Employee } from '../models/employee';
 
 @Component({
   selector: 'app-simpleform',
@@ -7,12 +8,25 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./simpleform.component.css']
 })
 export class SimpleformComponent implements OnInit {
+  defaultValue:string= "Angular"
+  employee = new Employee();
+  submitted: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
   OnSubmit(form:NgForm) {
-console.log(form);
+    this.submitted = true;
+    this.employee .course = form.value.course;
+    this.employee .userName = form.value.userDetails.username;
+    this.employee .email = form.value.userDetails.email;
+
+   this.saveEmployeeData(this.employee);
   }
+
+  saveEmployeeData(emp:Employee) {
+   console.log(emp.course);
+  }
+
 }
