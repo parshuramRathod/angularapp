8932af2a1b623c32d60ac38d1726108d59bff035
Parshuram <parshuramrathod9540@gmail.com>
2022-12-16 09:31:46 +0530

    learned nested form group
    learned nested form group

--- src/app/reactiveform/reactiveform.component.ts
+++ src/app/reactiveform/reactiveform.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-reactiveform',
@@ -7,44 +9,89 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./reactiveform.component.css']
 })
 export class ReactiveformComponent implements OnInit {
-  myReactiveForm: FormGroup;
-  notAllowedNames=['Codemind','Technology'];
-  genders= [
+  submitted: boolean = false;
+  notAllowedNames = ['Codemind', 'Technology'];
+  genders = [
     {
       id: '1',
-      value: 'male'
+      value: 'Male'
     },
     {
       id: '2',
       value: 'Female'
     }
   ]
-  constructor() {
+  myReactiveForm: FormGroup;
+  constructor() { 
     this.createForm();
-
   }
 
   ngOnInit() {
+    
+    // setTimeout(() => {
+    //   this.myReactiveForm.setValue({
+    //     'userDetails' : {
+    //       'username': 'Codemind1122',
+    //       'email': 'test@gmail.com'
+    //     },
+    //     'course': 'HTML',
+    //     'gender': 'Male'
+    //   })
+    // }, 3500);
+
+    // setTimeout(() => {
+    //   this.myReactiveForm.patchValue({
+    //     'userDetails' : {
+    //       'username': 'Codemind1122',
+    //       'email': 'test@gmail.com'
+    //     }
+    //   })
+    // }, 3500);
   }
 
-  createForm() {
+  createForm(){
     this.myReactiveForm = new FormGroup({
-      'username': new FormControl('', [Validators.required,this.NaNames.bind(this)]),
-      'email': new FormControl('', [Validators.required, Validators.email]),
-      'course': new FormControl('Angular'),
-      'gender': new FormControl('male')
+      'userDetails': new FormGroup({
+        'username': new FormControl('', [Validators.required, this.NaNames.bind(this)]),
+        'email': new FormControl('', [Validators.required, Validators.email], this.NaEmails)
+      }),
+      'course': new FormControl(''),
+      'gender': new FormControl(''),
+      'skills': new FormArray([
+       new FormControl(null, Validators.required)
+      ])
     })
-
   }
-
-  OnSubmit() {
+  OnSubmit()
+  {
+    this.submitted = true;
     console.log(this.myReactiveForm);
+    
+  }
+  OnAddSkills() {
+   (<FormArray>this.myReactiveForm.get('skills')).push(new FormControl(null, Validators.required));
+  }
+  NaNames(control:FormControl)
+  {  
+    //  this.notAllowedNames=   api/getNams 
+    if(this.notAllowedNames.indexOf(control.value) !== -1) {
+      return {'namesNotAllowed': true}
+    }
+    return null;
+  }
+  NaEmails(control:FormControl): Promise<any> | Observable<any> {
+    const myResponse = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if(control.value === 'codemindtechnology@gmail.com'){
+          resolve({'emailNotAllowed': true})
+        } else {
+          resolve(null)
+        }
+      }, 3000);
+    })
+    return myResponse;
+  }
 
-  }
-NaNames(control:FormControl){
-  if(this.notAllowedNames.indexOf(control.value)!==-1){
-    return {'nameNotAllowed':true}
-  }
-  return null;
-}
+  
+
 }
